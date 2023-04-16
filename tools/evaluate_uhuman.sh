9558969517020f118809ac177887ceec2c63b532
JimmyYZHU <jyuzhu@umich.edu>
2023-04-16 15:31:59 -0400

    uhuman

---
+++ tools/evaluate_uhuman.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+UHUMAN_PATH=/media/jimmy/TOSHIBA\ EXT/Data_Set/uhumans/apartment_scene
+
+evalset=(
+    left_images
+)
+
+for seq in ${evalset[@]}; do
+    echo $seq
+    python evaluation_scripts/test_uhuman.py --datapath=$UHUMAN_PATH/$seq --weights=droid.pth --reconstruction_path=uhuman_apartment$@
+done
+
